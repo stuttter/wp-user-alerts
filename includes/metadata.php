9f577f843cad87ef6452939cd6e19c280f93eade
John James Jacoby <johnjamesjacoby@me.com>
2015-09-21 09:23:29 -0500

    Remove ID. Will use post_parent.

--- includes/metadata.php
+++ includes/metadata.php
@@ -15,21 +15,9 @@ defined( 'ABSPATH' ) || exit;
  * @since 0.1.0
  */
 function wp_user_alerts_register_post_metadata() {
-	register_meta( 'post', 'wp_user_alert_id',         'wp_user_alerts_sanitize_id'         );
 	register_meta( 'post', 'wp_user_alert_types',      'wp_user_alerts_sanitize_types'      );
 	register_meta( 'post', 'wp_user_alert_methods',    'wp_user_alerts_sanitize_methods'    );
 	register_meta( 'post', 'wp_user_alert_severities', 'wp_user_alerts_sanitize_severities' );
-}
-
-/**
- * Sanitize user alert ID for saving
- *
- * @since 0.1.0
- *
- * @param array $id
- */
-function wp_user_alerts_sanitize_id( $id = 0 ) {
-	return absint( $id );
 }
 
 /**
