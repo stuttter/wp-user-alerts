ea1fcaa4dede6636ad9f9d59777770f1adb6d212
John James Jacoby <johnjamesjacoby@me.com>
2015-12-21 01:19:45 -0600

    Register user metadata.

--- includes/metadata.php
+++ includes/metadata.php
@@ -14,10 +14,16 @@ defined( 'ABSPATH' ) || exit;
  *
  * @since 0.1.0
  */
-function wp_user_alerts_register_post_metadata() {
+function wp_user_alerts_register_metadata() {
+
+	// Posts
 	register_meta( 'post', 'wp_user_alert_types',      'wp_user_alerts_sanitize_types'      );
 	register_meta( 'post', 'wp_user_alert_methods',    'wp_user_alerts_sanitize_methods'    );
 	register_meta( 'post', 'wp_user_alert_severities', 'wp_user_alerts_sanitize_severities' );
+
+	// Users
+	register_meta( 'user', 'cellular_number',  'wp_user_alerts_sanitize_cellular_number'  );
+	register_meta( 'user', 'cellular_carrier', 'wp_user_alerts_sanitize_cellular_carrier' );
 }
 
 /**
@@ -51,4 +57,33 @@ function wp_user_alerts_sanitize_methods( $methods = array() ) {
  */
 function wp_user_alerts_sanitize_severities( $severities = array() ) {
 	return $severities;
+}
+
+/**
+ * Sanitize user alert cellular number for saving
+ *
+ * @since 0.1.0
+ *
+ * @param array $number
+ */
+function wp_user_alerts_sanitize_cellular_number( $number = array() ) {
+	return $number;
+}
+
+/**
+ * Sanitize user alert cellular carrier for saving
+ *
+ * @since 0.1.0
+ *
+ * @param array $carrier
+ */
+function wp_user_alerts_sanitize_cellular_carrier( $carrier = array() ) {
+
+	// Get possible carriers
+	$carriers = array_keys( wp_user_alerts_get_cellular_carriers() );
+
+	// Return carrier if in array, or false if not
+	return in_array( $carrier, $carriers )
+		? $carrier
+		: false;
 }
