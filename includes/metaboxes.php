8788269158c7b0542817dfa505852619a0601868
John James Jacoby <johnjamesjacoby@me.com>
2016-04-01 13:41:00 -0500

    Add `form-table` class so Chosen targets correctly.

--- includes/metaboxes.php
+++ includes/metaboxes.php
@@ -458,7 +458,7 @@ function wp_user_alerts_users_picker( $args = array() ) {
 	$post  = get_post();
 	$_meta = wp_parse_id_list( get_post_meta( $post->ID, 'wp_user_alerts_user' ) ); ?>
 
-	<div id="alert-users" class="tabs-panel alerts-picker"<?php echo $args['visible']; ?>>
+	<div id="alert-users" class="form-table tabs-panel alerts-picker"<?php echo $args['visible']; ?>>
 		<select data-placeholder="<?php esc_html_e( 'Search...', 'wp-user-alerts' );?>" name="wp_user_alerts_users[]" id="<?php echo esc_attr( $args['post_type'] ); ?>-checklist" multiple="multiple"><?php
 
 			foreach ( $users as $user ) :
@@ -475,7 +475,7 @@ function wp_user_alerts_users_picker( $args = array() ) {
 
 			endforeach; ?>
 
-		</select>
+		</select>			
 	</div>
 
 	<?php
